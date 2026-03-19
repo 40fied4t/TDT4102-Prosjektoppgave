6fa624246913e1b76cd840fe919f6fc4e061b9f6
[Alfred Gilje Sanne] <[alfredgs@stud.ntnu.no]>
2026-03-19 20:06:27 +0100

    implementerer Node

--- include/graph.h
+++ include/graph.h
@@ -4,25 +4,30 @@
 #include <vector>
 #include <unordered_map>
 #include <filesystem>
+#include <fstream>
 
 #include "AnimationWindow.h"
 //Egendefinerte typer
-typedef std::vector<std::shared_ptr<Node>> NodeVec;
-typedef std::unordered_map<std::shared_ptr<Node>, NodeVec> GraphMap;
-typedef std::vector<std::vector<std::shared_ptr<Node>>> EdgeVec;
+// typedef std::vector<std::shared_ptr<Node>> NodeVec;
+// typedef std::unordered_map<std::shared_ptr<Node>, std::vector<std::shared_ptr<Node>>> GraphMap;
+// typedef std::vector<std::vector<std::shared_ptr<Node>>> EdgeVec;
 
 class Graph
 {
 private:
-    int numNodes;
-    int nextLabel;
-    GraphMap graphMap;
-    EdgeVec edgeVec;
-    NodeVec nodes;
-    NodeVec selectedNodes;
+    static constexpr int width = 1000;
+    static constexpr int height = 500;
+
+    int numNodes = 0;
+    int nextLabel = 1;
+    std::unordered_map<std::shared_ptr<Node>, std::vector<std::shared_ptr<Node>>> graphMap;
+    std::vector<std::vector<std::shared_ptr<Node>>> edgeVec;
+    std::vector<std::shared_ptr<Node>> nodes;
+    std::vector<std::shared_ptr<Node>> selectedNodes;
 
 public:
     void updateSelectedNodes();
+    void updateNextLabel();
     void addEdges();
     void removeEdges();
     void addNode(const TDT4102::Point location, const int& label);
@@ -32,7 +37,7 @@ public:
     int getEdgeNum();
 
 
-    Graph();
+    Graph() = default;
     Graph(std::filesystem::path fileName);
     ~Graph();
 
@@ -51,6 +56,8 @@ private:
     bool selected = false;
     TDT4102::Point  location;
 public:
+    Node(TDT4102::Point location, const int& label);
+    ~Node();
     TDT4102::Point getLocation() const;
     int getLabel() const;
     bool isSelected() const;
--- src/data/graph.cpp
+++ src/data/graph.cpp
@@ -1,1 +1,47 @@
 #include "graph.h"
+//implementer Node
+Node::Node(TDT4102::Point location, const int& label):
+    label{label},
+    location{location}
+{}
+TDT4102::Point Node::getLocation() const {
+    return location;
+}
+int Node::getLabel() const {
+    return label;
+}
+bool Node::isSelected() const {
+    return selected;
+}
+void Node::updateSelect() {
+    selected = !selected;
+}
+
+
+//implementer Graph
+void Graph::updateSelectedNodes(){
+    for (auto it = nodes.begin(); it != nodes.end();++it) {
+
+    }
+}
+void Graph::addEdges(){}
+void Graph::removeEdges(){}
+void Graph::addNode(const TDT4102::Point location, const int& label){}
+void Graph::removeNode(){}
+void Graph::removeSelectedNodes(){}
+int Graph::getSize(){}
+int Graph::getEdgeNum(){}
+
+
+Graph::Graph(){
+    addNode({width/2, height/2}, nextLabel);
+    updateNextLabel();
+}
+Graph::Graph(std::filesystem::path fileName){}
+Graph::~Graph(){}
+
+void Graph::loadFromAdj(std::filesystem::path fileName){}
+void Graph::loadFromEdg(std::filesystem::path fileName){}
+
+void Graph::saveToAdj(std::filesystem::path fileName){}
+void Graph::saveToEdg(std::filesystem::path fileName){}
